c75b6fc6344e9c14210d4d928c39fda1829a6c36
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2023-01-16 09:57:35 +0530

    changing white to grey

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from '../../styles/Home.module.scss'
+import Head from 'next/head'
+ 
+export default function Home() {
+return (
+  <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+    <h1 className={styles.title}>
+        Hello World
+      </h1>
+  </>
+  
+)
+}
