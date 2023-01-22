acf5025eb509ec57166c0bbd7872ee47c1b33c37
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2023-01-22 18:19:04 +0530

    home page editing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app'
-import '../../styles/globals.scss'
+import { Header } from '../components/Header'
+import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
+  )
+  
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import styles from '../../styles/Home.module.scss'
 import Head from 'next/head'
+import styles from './home.module.scss'
  
 export default function Home() {
 return (
@@ -7,9 +7,19 @@ return (
     <Head>
       <title>Home | ig.news</title>
     </Head>
-    <h1 className={styles.title}>
-        Hello World
-      </h1>
+
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 monthly</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl codign" />
+    </main>
   </>
   
 )
