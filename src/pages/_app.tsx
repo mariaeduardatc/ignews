14069ef69a24b3afe31be537bcccae1a36f1feb6
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2023-01-24 16:59:14 +0530

    oauth set

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { SessionProvider as NextAuthProvider } from 'next-auth/react'
+
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header/>
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
   
 }
