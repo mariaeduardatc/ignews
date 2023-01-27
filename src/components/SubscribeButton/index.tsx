c00160d9fa518788f91d7c292ca0a701db60c2e1
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2023-01-27 18:32:48 +0530

    more additions to the api route, fauna, no repeat customers

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss"
 
 interface SubscribeButtonProps {
@@ -6,10 +9,33 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId} : SubscribeButtonProps){
+    const session = useSession();
+
+    async function handleSubscribe(){
+        // if there is no user session
+        if(!session){
+            signIn('github')
+            return; 
+        }
+        // create checkout session
+        try{
+            const response = await api.post('/subscribe')
+    
+            const {sessionId} = response.data;
+    
+            const stripe = await getStripeJs()
+    
+            await stripe!.redirectToCheckout(sessionId)
+        }catch(err){
+            alert(err.message)
+        }
+        
+    }
     return(
         <button 
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe
         </button>
